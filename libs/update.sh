009805c37975e771cf33ff38501d93c9b3073248
Marcel Block <MarcelBlock@gmail.com>
2024-12-26 14:10:41 +0100

    Updated dependency-download-script

--- libs/update.sh
+++ libs/update.sh
@@ -6,3 +6,6 @@ git clone https://github.com/zig-gamedev/zopengl.git
 git clone https://github.com/zig-gamedev/zmath.git
 git clone https://github.com/zig-gamedev/zgui.git
 git clone https://github.com/zig-gamedev/zstbi.git
+
+git clone https://github.com/erincatto/box2d.git   zbox/libs/box2d
+git clone https://github.com/mapbox/earcut.hpp.git zearcut/libs/earcut.hpp
